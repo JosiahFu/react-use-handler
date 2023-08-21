26a99c324c045a47400dccca1ab6db26d9b13675
JosiahFu <josiahfu@gmail.com>
2023-08-20 23:16:23 -0700

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # `react-use-handler`
+
+A react hook to set global/arbitrary event handlers.
+
+## Example Usage
+
+```ts
+useHandler('click', useCallback(() => closeDialog(), []));
+// It is recommended to wrap the function with useCallback to prevent the
+// function from being changed every render
+```
+
+Using an event target other than `window`:
+
+```ts
+useHandler('resize', useCallback(() => fixSizing(), []), document);
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { useEffect } from 'react';
 
+/**
+ * Set a handler on an arbitrary target
+ *
+ * @param type The type of event to listen for.
+ * @param callback The event handler.
+ * @param target The event target, `window` if not specified.
+ * @param options The options for `addEventListener`.
+ */
 function useHandler<K extends keyof GlobalEventHandlersEventMap, T extends GlobalEventHandlers>(type: K, callback: (event: GlobalEventHandlersEventMap[K]) => any, target: T = (window as unknown) as T, options?: boolean | AddEventListenerOptions) {
     useEffect(() => {
         target.addEventListener(type, callback, options);
